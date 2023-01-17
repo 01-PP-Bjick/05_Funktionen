8489959e87bfe3629dbd6f914fa3a391be1688f4
bjick84 <bjick@web.de>
2023-01-17 11:10:12 +0100

    Pause variable in der funktion ++

--- script_01.js
+++ script_01.js
@@ -3,13 +3,24 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
-// Funktionsaufruf (call)
-test();
+// Funktionsaufruf (call) Anzahl bestimmt häufigkeit
+// test();
 
 // Funktionsrumpf (body) | callee
 // Funktionsdeklaration
-
 function test()
 {
     console.log("Hallo Patrick!");
+}
+
+/***** Funktion 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+ausgabeNamen();
+// console.log(firstName); // Fehler: Scope!
+
+function ausgabeNamen() 
+{
+    let firstName = "Patrick";  // nur innerhalb der Funktion erreichbar
+    console.log("Hallo " + firstName + "!");
 }
